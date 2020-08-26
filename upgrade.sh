eeee8faa82962aea51c47b18f55a4abca8174037
rpanak-generalbytes <31093482+rpanak-generalbytes@users.noreply.github.com>
2020-08-26 13:45:50 +0200

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -2,13 +2,14 @@
 set -exo pipefail
 
 dockerComposeFile="docker-compose.yml"
-dockerComposeFileSha256="0e59080b852118c3392da0ed9eb7f67abdb5885172c4060e9eedc1de9c68957e"
+dockerComposeFileCommit="3a4899b6f6d2cd487469f3c6b8dc10d84e664b22"
+dockerComposeFileSha256="175bcfd2bfc651563781451140946b81bbb5dc1f8f71ba992b1a259e72b4bebf"
 if [[ -f "$dockerComposeFile" ]]; then
     >&2 echo "$dockerComposeFile already exists."
     exit 2
 fi
 
-curl "https://raw.githubusercontent.com/everytrade-io/everytrade-install/01373871596d0e46d724b960fbadde73a0a79130/docker-compose.yml" -o "$dockerComposeFile"
+curl "https://raw.githubusercontent.com/everytrade-io/everytrade-install/$dockerComposeFileCommit/docker-compose.yml" -o "$dockerComposeFile"
 sha256sum "$dockerComposeFile" | grep "$dockerComposeFileSha256"
 sudo docker-compose pull
 sudo docker-compose up -d
