f3f8563c98eb546c4c29119b29987fd9da777ebe
Radovan Panak <radovan.panak@generalbytes.com>
2020-08-31 19:27:17 +0200

    change scripts to use latest version

--- upgrade.sh
+++ upgrade.sh
@@ -2,8 +2,8 @@
 set -exo pipefail
 
 dockerComposeFile="docker-compose.yml"
-dockerComposeFileCommit="3a4899b6f6d2cd487469f3c6b8dc10d84e664b22"
-dockerComposeFileSha256="175bcfd2bfc651563781451140946b81bbb5dc1f8f71ba992b1a259e72b4bebf"
+dockerComposeFileCommit="master"
+dockerComposeFileSha256="f789db6d327d0664e452cca266fb197d3d6baa3d4c5899ef5b2d78af2b052052"
 if [[ -f "$dockerComposeFile" ]]; then
     >&2 echo "$dockerComposeFile already exists."
     exit 2
