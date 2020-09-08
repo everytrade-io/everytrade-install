670b6079ebde598d41ae4fecdc64f810ebc217fa
Radovan Panak <radovan.panak@generalbytes.com>
2020-09-08 17:57:29 +0200

    - change image names - scripts can override compose image name and/or version

--- install.sh
+++ install.sh
@@ -1,9 +1,38 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+while [[ $# -gt 0 ]]; do
+    arg="$1"
+    case $arg in
+        --host)
+            host="$2"
+            shift # past argument
+            shift # past value
+        ;;
+        --image)
+            image="$2"
+            shift # past argument
+            shift # past value
+        ;;
+        --version)
+            version="$2"
+            shift # past argument
+            shift # past value
+        ;;
+    esac
+done
+
+if [[ -z "$host" ]]; then
+    host=$(ip route get 8.8.8.8 | head -1 | awk '{print $7}')
+fi
+
+if [[ -z "$image" ]]; then
+    image="everytrade-webapp"
+fi
+
 dockerComposeFile="docker-compose.yml"
 dockerComposeFileCommit="master"
-dockerComposeFileSha256="f789db6d327d0664e452cca266fb197d3d6baa3d4c5899ef5b2d78af2b052052"
+#dockerComposeFileSha256="f789db6d327d0664e452cca266fb197d3d6baa3d4c5899ef5b2d78af2b052052"
 if [[ -f "$dockerComposeFile" ]]; then
     >&2 echo "$dockerComposeFile already exists."
     exit 1
@@ -22,10 +51,6 @@ if [[ ! "$key" =~ ^[0-9a-zA-Z]{64}$ ]]; then
     exit 1
 fi
 
-host="$1"
-if [[ -z "$host" ]]; then
-    host=$(ip route get 8.8.8.8 | head -1 | awk '{print $7}')
-fi
 username="user-${key:0:32}"
 password="${key:32:32}"
 
@@ -33,8 +58,15 @@ sudo apt-get update
 sudo apt-get -y install docker.io docker-compose nginx certbot python3-certbot-nginx
 #sudo usermod -a -G docker "$USER"
 
-curl "https://raw.githubusercontent.com/everytrade-io/everytrade-install/$dockerComposeFileCommit/docker-compose.yml" -o "$dockerComposeFile"
-sha256sum "$dockerComposeFile" | grep "$dockerComposeFileSha256"
+curl "https://raw.githubusercontent.com/everytrade-io/everytrade-install/$dockerComposeFileCommit/docker-compose.yml" | \
+  (
+      if [[ -z "$version" ]]; then
+          sed -e "s/^\(\s*image: registry\.everytrade\.io\/\)everytrade-webapp:\(.*\)$/\1$image:\2/"
+      else
+          sed -e "s/^\(\s*image: registry\.everytrade\.io\/\)everytrade-webapp:\(.*\)$/\1$image:$version/"
+      fi
+  ) > "$dockerComposeFile"
+#sha256sum "$dockerComposeFile" | grep "$dockerComposeFileSha256"
 echo "$password" | sudo docker login -u "$username" --password-stdin registry.everytrade.io
 sudo docker-compose pull
 sudo EVERYTRADE_INSTALL_HOST="$host" docker-compose up -d
