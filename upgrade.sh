ccc2e0de109496474caeb7d8318826e0fa9cbf31
kubacech <kubacech@gmail.com>
2022-10-06 13:06:04 +0200

    updating to new build

--- upgrade.sh
+++ upgrade.sh
@@ -23,7 +23,6 @@ fi
 
 dockerComposeFile="docker-compose.yml"
 dockerComposeFileCommit="master"
-#dockerComposeFileSha256="f789db6d327d0664e452cca266fb197d3d6baa3d4c5899ef5b2d78af2b052052"
 if [[ -f "$dockerComposeFile" ]]; then
     >&2 echo "$dockerComposeFile already exists."
     exit 2
